ddff3cae75d1e5c05126ff1b26d590ddf2b001ca
Ignat Senchenkov <sencenkov291@gmail.com>
2026-03-16 12:00:01 +0300

    lol

--- program.py
+++ program.py
@@ -1,1 +1,21 @@
-print('Я домашка')
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        # Добавьте сюда атрибут "скидка" со значением по умолчанию 10.
+        self.__discount = 10
+
+    # Реализуйте методы get_price() и set_discount().
+    def get_price(self, price):
+        return round(price + (price * (self.__discount / 100)), 2)
+
+    def set_discount(self, discount):
+        if discount > 80:
+            self.__discount = 80
+        else:
+            self.__discount = discount
+
+
+customer = Customer("Иван Иванович")
+print(customer.get_price(100))
+print(customer.set_discount(20))
+print(customer.get_price(100))
